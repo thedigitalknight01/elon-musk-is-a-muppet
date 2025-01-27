7abbf4ea3c76ec48ddc00d60ea8e368da2f1bbb0
Alan Smith <alansmith@Mac.fritz.box>
2025-01-27 09:41:22 +0000

    Added coffee tips button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   muskyImage = 'https://media.licdn.com/dms/image/v2/D4D12AQHji9WB6ImiGg/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1704609884366?e=1743033600&v=beta&t=C11H693Da1kjcxtE8rqGI8aqGjMGGjzSuaT7UFfEPL0';
+  digitalKnightImage = 'images/the-digital-knight.jpeg';
+  
   links: { title: string; url: string }[] = [];
 
   constructor(private httpClient: HttpClient) { }
