777fcaedd78af0c5afdcc8912572a08129e2da29
Alan Smith <thedigitalknight01@gmail.com>
2025-03-04 17:07:04 +0000

    Minor updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  muskyImage = 'https://media.licdn.com/dms/image/v2/D4D12AQHji9WB6ImiGg/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1704609884366?e=1743033600&v=beta&t=C11H693Da1kjcxtE8rqGI8aqGjMGGjzSuaT7UFfEPL0';
-  digitalKnightImage = 'images/the-digital-knight.jpeg';
+  muppetImage = 'images/muppet.avif';
   
   links: { title: string; url: string }[] = [];
 
